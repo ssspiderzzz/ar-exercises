388f4807bb91ad3b3380e136090a6aa09cd9490f
ssspiderzzz <ssspiderzzz@gmail.com>
2019-08-19 21:25:02 +0000

    add 3 stores, puts stores carry mens apparel

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,13 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+Store.create(name: "Surrey", annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
+Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
+Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
+
+@mens_stores = Store.where(mens_apparel: true)
+
+puts "These are stores carry mans apparel:"
+@mens_stores.each do |i|
+  puts "Store #{i.name}'s annual revenue is#{i.annual_revenue}."
+end
